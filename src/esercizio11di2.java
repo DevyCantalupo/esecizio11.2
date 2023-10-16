c03d34a135804346902159f41551d172e2947978
Devy Franco Cantalupo <devycantalupo@outlook.com>
2023-10-16 10:26:13 +0200

    First Commit

---
+++ src/esercizio11di2.java
@@ -0,0 +1,10 @@
+public class esercizio11di2 {
+    public static void resta(int limite, int inicio){
+        for(int i = inicio; i > limite; i--){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+            resta(3,6);
+    }
+}
